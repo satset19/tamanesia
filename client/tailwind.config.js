1cc9231db09ee65b9ef526a2b662286b01061b3d
satset19 <laksonosatiyo@gmail.com>
2023-10-06 23:35:10 +0700

    a

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+import { nextui } from '@nextui-org/theme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: {
+          1: '#2D2F91'
+        }
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
